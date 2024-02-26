d5491aff2a49608235e31b143cad699d57c9da72
Bobur Sobirjanov <sobirjanovbobur768@gmail.com>
2024-02-26 22:26:14 +0500

    security added

---
+++ src/main/java/uz/com/ecommers/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package uz.com.ecommers.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import uz.com.ecommers.filter.FilterToken;
+import uz.com.ecommers.service.auth.AuthenticationService;
+import uz.com.ecommers.service.auth.JwtService;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity()
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+    private final String[] permitAll = {"/swagger-ui/**", "/v3/api-docs/**", "/api/v1/auth/**"};
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requestsConfigurer) ->
+                        requestsConfigurer
+                                .requestMatchers(permitAll).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new FilterToken(authenticationService, jwtService), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8081/api/v1", "*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of("http://localhost:8081/api/v1","*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+
+
+    }
+}
--- src/main/java/uz/com/ecommers/service/CardService.java
+++ src/main/java/uz/com/ecommers/service/CardService.java
@@ -2,8 +2,12 @@ package uz.com.ecommers.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.com.ecommers.repository.CardRepository;
+import uz.com.ecommers.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CardService {
+    private final CardRepository cardRepository;
+    private final UserRepository userRepository;
 }
