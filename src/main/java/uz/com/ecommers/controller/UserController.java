ddf173ab4c3b169ad98347074d2640bb21f46478
Bobur Sobirjanov <sobirjanovbobur768@gmail.com>
2024-03-01 20:01:37 +0500

    categories have changed

--- src/main/java/uz/com/ecommers/controller/UserController.java
+++ src/main/java/uz/com/ecommers/controller/UserController.java
@@ -7,6 +7,9 @@ import uz.com.ecommers.model.dto.user.UserForUser;
 import uz.com.ecommers.response.StandardResponse;
 import uz.com.ecommers.service.UserService;
 
+import java.security.Principal;
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
@@ -20,5 +23,36 @@ public class UserController {
             @RequestParam String email
     ){
         return userService.assignEmployer(email);
+    }
+    @PostMapping("/assign-employer")
+    @PreAuthorize("hasRole('OWNER')")
+    public StandardResponse<UserForUser> assignToAdmin(
+            @RequestParam String email
+    ){
+        return userService.assignToAdmin(email);
+    }
+
+    @DeleteMapping("/delete")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('OWNER')")
+    public StandardResponse<String> deleteUser(
+            @RequestParam String email,
+            Principal principal
+    ){
+        return userService.deleteUser(email,principal);
+    }
+
+    @PostMapping("/remove-admin")
+    @PreAuthorize("hasRole('OWNER')")
+    public StandardResponse<UserForUser> removeAdmin(
+            @RequestParam String email
+    ){
+        return userService.removeAdmin(email);
+    }
+
+    @GetMapping("/{id}/get-by-id")
+    public StandardResponse<UserForUser> getById(
+            @PathVariable UUID id
+            ){
+        return userService.getUserById(id);
     }
 }
--- src/main/java/uz/com/ecommers/service/CategoryService.java
+++ src/main/java/uz/com/ecommers/service/CategoryService.java
@@ -3,24 +3,25 @@ package uz.com.ecommers.service;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import uz.com.ecommers.exception.DataNotFoundException;
 import uz.com.ecommers.exception.NotAcceptableException;
 import uz.com.ecommers.model.dto.category.CategoryDto;
 import uz.com.ecommers.model.dto.category.CategoryForUser;
 import uz.com.ecommers.model.entity.product.ProductCategory;
 import uz.com.ecommers.model.entity.user.UserEntity;
 import uz.com.ecommers.repository.CategoryRepository;
-import uz.com.ecommers.repository.ProductRepository;
 import uz.com.ecommers.repository.UserRepository;
 import uz.com.ecommers.response.StandardResponse;
 import uz.com.ecommers.response.Status;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
-    private final ProductRepository productRepository;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
 
@@ -41,5 +42,42 @@ public class CategoryService {
                 .message("Category saved!")
                 .data(categoryForUser)
                 .build();
+    }
+
+    public StandardResponse<String> delete(String name,Principal principal){
+        UserEntity user = userRepository.findUserEntityByEmail(principal.getName());
+        ProductCategory category = categoryRepository.findByName(name);
+        if (category==null){
+            throw new DataNotFoundException("Category not found!");
+        }
+        category.setDeleted(true);
+        category.setDeletedBy(user.getId());
+        category.setDeletedTime(LocalDateTime.now());
+        categoryRepository.save(category);
+        return StandardResponse.<String>builder()
+                .status(Status.SUCCESS)
+                .message("Deleted category")
+                .data("DELETED SUCCESSFULLY!")
+                .build();
+    }
+    public StandardResponse<CategoryForUser> getByName(String name){
+        ProductCategory category = categoryRepository.findByName(name);
+        if (category==null){
+            throw new DataNotFoundException("Category not found!");
+        }
+        CategoryForUser categoryForUser = modelMapper.map(category, CategoryForUser.class);
+        return StandardResponse.<CategoryForUser>builder()
+                .status(Status.SUCCESS)
+                .message("This is category!")
+                .data(categoryForUser)
+                .build();
+    }
+
+    public List<ProductCategory> getAll(){
+        List<ProductCategory> productCategories = categoryRepository.getAll();
+        if (productCategories==null){
+            throw new DataNotFoundException("Categories not found");
+        }
+        return productCategories;
     }
 }
